f07a22de36f2097307c68ab0071551ac9c8c6adc
Ana Ausek <anac_ausek@hotmail.com>
2017-07-14 12:21:30 -0400

    first push

---
+++ pillbox/app/Http/Controllers/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function admin()
+    {
+    	return "<h2>Admin view</h2>";
+    }
+}
---
+++ pillbox/resources/views/pages/scripts.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Your Dashboard</div>
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Prescription #</th>
+                            <th>Medication</th>
+                            <th>Refills Remaining</th>
+                        </tr>
+                    </table>
+                <div class="panel-body">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
